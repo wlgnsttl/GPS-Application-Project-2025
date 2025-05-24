8c8c089ec438b8f0d741e3db525b58e63ce8ebca
Hunyeap <12201311@inha.edu>
2025-05-24 16:00:45 +0900

    가중치 (Snr)/(MaxSnr) 형태로 변경

--- functions/WeightSNR.m
+++ functions/WeightSNR.m
@@ -1,6 +1,7 @@
-function W_snr = WeightSNR(matrix_snr)
+function W_snr = WeightSNR(matrix_snr,MaxSnr)
 
 matrix_snr(abs(matrix_snr) < 30) = 0;
-matrix_snr = matrix_snr./100;
+matrix_snr = matrix_snr./MaxSnr;
 
-W_snr = diag(matrix_snr(:,1));
+W_snr = diag(matrix_snr(:,1)) .* diag(matrix_snr(:,1));
+end
